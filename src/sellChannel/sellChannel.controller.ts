db2b96bb1d62329e49a90317f8b89a5a2d5b3b04
koladiya-nikunj <koladiyanikunj124@gmail.com>
2024-02-02 11:31:23 +0530

    Input field sales_id completed with validation

--- src/sellChannel/sellChannel.controller.ts
+++ src/sellChannel/sellChannel.controller.ts
@@ -7,16 +7,16 @@ export class SellChannelController {
   constructor(private readonly sellChannelService: SellChannelService) {}
 
   @Get()
-  async getApi(@Body() body: { salies_id: string }) {
+  async getApi(@Body() body: { sales_id: string }) {
     try {
-      if (!body.salies_id) {
+      if (!body.sales_id) {
         throw new Error('salesId must not be empty in the request body');
       }
-      // Check if salies_id is a string
-      if (typeof body.salies_id !== 'string') {
+      // Check if sales_id is a string
+      if (typeof body.sales_id !== 'string') {
         throw new Error('salesId must be a string in the request body');
       }
-      const userData = await this.sellChannelService.getDataFromMySQLToMongo(body.salies_id);
+      const userData = await this.sellChannelService.getDataFromMySQLToMongo(body.sales_id);
 
       // Use userData to do further processing or return it in the response
       return userData;
